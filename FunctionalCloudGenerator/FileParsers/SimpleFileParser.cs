4e42b31a1c3cd21a304be17327569a4d00b546f5
Dodo888 <darkdodo888@gmail.com>
2015-12-13 21:56:46 +0500

    Simple File Parser
    Added new file parser - simple (or dictionary)

---
+++ FunctionalCloudGenerator/FileParsers/SimpleFileParser.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FunctionalCloudGenerator.FileParsers
+{
+    public static class SimpleFileParser
+    {
+        public static Dictionary<string, int> GetWordsDictionaryFromFile(string path)
+        {
+            var wordsDictionary = new Dictionary<string, int>();
+            using (var file = new StreamReader(path))
+            {
+                string line;
+                while ((line = file.ReadLine()) != null)
+                {
+                    line = line.ToLower();
+                    if (wordsDictionary.ContainsKey(line))
+                    {
+                        wordsDictionary[line]++;
+                    }
+                    else
+                    {
+                        wordsDictionary.Add(line, 1);
+                    }
+                }
+            }
+            return wordsDictionary;
+        }
+    }
+}
