1eeebb70f82ca01d09987f0fb683becd84f52a61
Dodo888 <darkdodo888@gmail.com>
2015-12-13 21:55:30 +0500

    Cloud Drawers
    Added classes that draw clouds

---
+++ FunctionalCloudGenerator/CloudDrawers/RandomCloudDrawer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace FunctionalCloudGenerator.CloudDrawers
+{
+    public static class RandomCloudDrawer
+    {
+        private const int MaxTriesAmount = 100;
+        private static readonly Random Random = new Random();
+
+        public static Bitmap FormCloud(Bitmap bitmap, Configuration config, List<string> words)
+        {
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                var fontSizeDecrease = 0;
+                var occupiedAreas = new HashSet<RectangleF>();
+                graphics.Clear(ColorTranslator.FromHtml(config.BackgroundColor));
+                foreach (var word in words)
+                {
+                    occupiedAreas = DrawWord(graphics, config, word, fontSizeDecrease, occupiedAreas);
+                    fontSizeDecrease++;
+                }
+            }
+            return bitmap;
+        }
+
+        private static HashSet<RectangleF> DrawWord(Graphics graphics, Configuration config,
+            string word, int fontSizeDecrease, HashSet<RectangleF> occupiedAreas)
+        {
+            var font = new Font(config.Font, Math.Max(config.MaxFontSize - 3 * fontSizeDecrease, config.MinFontSize));
+            SizeF textSize = graphics.MeasureString(word, font);
+            Point textPlace = DefineArea(occupiedAreas, textSize, new Point(config.Width, config.Height));
+            graphics.DrawString(word, font,
+                new SolidBrush(ColorTranslator.FromHtml(config.Colors[Random.Next(config.Colors.Count)])),
+                textPlace);
+            occupiedAreas.Add(new RectangleF(textPlace, textSize));
+            return occupiedAreas;
+        }
+
+        public static Point DefineArea(HashSet<RectangleF> occupiedAreas, SizeF textSize, Point bitmap)
+        {
+            var random = new Random();
+            var hasStartPoint = false;
+            var offsetX = 0;
+            var offsetY = 0;
+            var triesAmount = 0;
+            while (!hasStartPoint || HasIntersection(occupiedAreas, offsetX, offsetY, textSize))
+            {
+                if (triesAmount > MaxTriesAmount)
+                    break;
+                offsetX = random.Next(bitmap.X - (int)textSize.Width);
+                offsetY = random.Next(bitmap.Y - (int)textSize.Height);
+                triesAmount++;
+                hasStartPoint = true;
+            }
+            return new Point(offsetX, offsetY);
+        }
+
+        public static bool HasIntersection(HashSet<RectangleF> occupiedAreas, int offsetX, int offsetY, SizeF textSize)
+        {
+            var anotherArea = new RectangleF(new PointF(offsetX, offsetY), textSize);
+            return occupiedAreas.Any(area => area.IntersectsWith(anotherArea));
+        }
+    }
+}
---
+++ FunctionalCloudGenerator/CloudDrawers/SimpleCloudDrawer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace FunctionalCloudGenerator.CloudDrawers
+{
+    public static class SimpleCloudDrawer
+    {
+        public static Bitmap FormCloud(Bitmap bitmap, Configuration config, List<string> words)
+        {
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                PointF offset = new PointF(0, 0);
+                float maxOffsetForNextColumn = 0;
+                graphics.Clear(ColorTranslator.FromHtml(config.BackgroundColor));
+                for (var i = 0; i < words.Count; i++)
+                {
+                    var font = new Font(config.Font, Math.Max(config.MaxFontSize - 2 * i, config.MinFontSize));
+                    SizeF textSize = graphics.MeasureString(words[i], font);
+                    offset = CountOffset(offset, textSize, bitmap.Height, maxOffsetForNextColumn);
+                    maxOffsetForNextColumn = (Math.Abs(offset.Y) < 0.1) ? 0 : maxOffsetForNextColumn;
+                    if (offset.X >= bitmap.Width)
+                        break;
+                    graphics.DrawString(words[i], font,
+                        new SolidBrush(ColorTranslator.FromHtml(config.Colors[i % config.Colors.Count])),
+                        offset);
+                    offset.Y += textSize.Height;
+                    maxOffsetForNextColumn = Math.Max(maxOffsetForNextColumn, textSize.Width);
+                }
+            }
+            return bitmap;
+        }
+
+        private static PointF CountOffset(PointF oldOffset, SizeF textSize, int height, float maxOffset)
+        {
+            return (oldOffset.Y + textSize.Height > height) ?
+                new PointF(oldOffset.X + maxOffset, 0) : oldOffset;
+        }
+    }
+}
