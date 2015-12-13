11be3e7c67336c1970119c95fb25a415b22d4d71
Dodo888 <darkdodo888@gmail.com>
2015-12-13 21:56:03 +0500

    Console Application
    Added new application type - console

---
+++ FunctionalCloudGenerator/ApplicationTypes/ConsoleApplication.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace FunctionalCloudGenerator.ApplicationTypes
+{
+    public static class ConsoleApplication
+    {
+        public static void CreateImage(Bitmap bitmap, string imagePath, ImageFormat format)
+        {
+                bitmap.Save(imagePath, format);
+        }
+    }
+}
